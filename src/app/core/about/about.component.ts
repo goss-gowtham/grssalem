a9684e94ed4cf42cd96d121a7a4101839b7a432d
Gowthamnarayanan S <gossgowtham@gmail.com>
2025-09-01 16:49:01 +0530

    About Section removed

--- src/app/core/about/about.component.ts
+++ src/app/core/about/about.component.ts
@@ -17,23 +17,24 @@ export class AboutComponent implements OnInit {
     Solutions" to build a strong technical team for providing solutions to
     analytical testing, Operation & Maintenance and technology providing
     services.`,
-  }, {
-    name: 'Vijayakumar Palanimuthu',
-    desg: 'CEO',
-    imgSrc: '../../../assets/Vijayakumar.JPG',
-    desc: `Passionate about waste to energy technology and environmental solutions.
-    Having 14years of technical experience in various plant operation &
-    maintenance activities and optimization of various waste to energy
-    plants. Scientific and technical knowledge in the waste to energy and
-    environmental business and held the position of C.O.O. at Mahavir Eco
-    Projects, Surat in 2017-2021. <br/>Has considerable experience in formulating
-    O&M procedures, risk assessment, ISO standards, external contractors,
-    cost control, budget proposal and safe work practices. Involved in
-    southeast Asia largest waste to energy plant at Recycle Energy Sdn Bhd,
-    Malaysia in 2012-2014 handled O&M activities and optimization of
-    hazardous waste projects. Completed Executive Development Program in
-    Operations Management at IIM, Kashipur.`,
-  }, ];
+  }];
+  // , {
+  //   name: 'Vijayakumar Palanimuthu',
+  //   desg: 'CEO',
+  //   imgSrc: '../../../assets/Vijayakumar.JPG',
+  //   desc: `Passionate about waste to energy technology and environmental solutions.
+  //   Having 14years of technical experience in various plant operation &
+  //   maintenance activities and optimization of various waste to energy
+  //   plants. Scientific and technical knowledge in the waste to energy and
+  //   environmental business and held the position of C.O.O. at Mahavir Eco
+  //   Projects, Surat in 2017-2021. <br/>Has considerable experience in formulating
+  //   O&M procedures, risk assessment, ISO standards, external contractors,
+  //   cost control, budget proposal and safe work practices. Involved in
+  //   southeast Asia largest waste to energy plant at Recycle Energy Sdn Bhd,
+  //   Malaysia in 2012-2014 handled O&M activities and optimization of
+  //   hazardous waste projects. Completed Executive Development Program in
+  //   Operations Management at IIM, Kashipur.`,
+  // }, ];
 
   constructor() { }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
+
 })
 export class HeaderComponent implements OnInit {
 
